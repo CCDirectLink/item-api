00c647fb0c0332eaca3c832dbffe310c16e46afd
Jonathan Sun <bakafish@berkeley.edu>
2020-02-29 08:11:18 -0800

    More injections to fix trader display issues.

--- base-ps.js
+++ base-ps.js
@@ -22,17 +22,17 @@ sc.Inventory.inject({
 		var f = window.itemAPI.customItemToId[b];
 		f && (a = f);
         if (!(a < 0)) {
-            this.items[a] = this.items[a] ? Math.min(this.items[a] + (c | 0), 99) : c | 0;
-            this._addNewItem(a);
-            sc.stats.addMap("items", "total", c);
-            sc.stats.addMap("items", a, c);
-            b.id = a;
-            b.amount = c;
-            b.skip = d;
-            b.cutscene = e;
-            sc.Model.notifyObserver(this, sc.PLAYER_MSG.ITEM_OBTAINED,
-                b)
-        }
+			this.items[a] = this.items[a] ? Math.min(this.items[a] + (c | 0), 99) : c | 0;
+			this._addNewItem(a);
+			sc.stats.addMap("items", "total", c);
+			sc.stats.addMap("items", a, c);
+			b.id = a;
+			b.amount = c;
+			b.skip = d;
+			b.cutscene = e;
+			sc.Model.notifyObserver(this, sc.PLAYER_MSG.ITEM_OBTAINED,
+				b)
+		}
     },
     isBuffID: function(b) {
 		return this.getItem(b).isBuff
@@ -65,7 +65,7 @@ sc.Inventory.inject({
 	isConsumable: function(b) {
 		b = this.getItem(b);
 		return !b ? false : b.type == sc.ITEMS_TYPES.CONS
-	},
+	}
 });
 
 // Devs be violating abstraction barriers I swear to god.
@@ -74,5 +74,44 @@ sc.ItemContent.inject({
 		var c = window.itemAPI.customItemToId[b];
 		c && (b = c);
 		this.parent(b, a);
+	}
+});
+
+sc.PlayerModel.inject({
+	getItemAmount: function(a) {
+		var b = window.itemAPI.customItemToId[a];
+		b && console.log(`${a} -> ${b}: ${this.items[b]}`);
+		b && (a = b);
+        if (!(a < 0)) return this.items[a] || 0
+    },
+	getItemAmountWithEquip: function(a) {
+		var b = window.itemAPI.customItemToId[a];
+		b && console.log(`(equip) ${a} -> ${b}: ${this.items[b]}`);
+		b && (a = b);
+		if (!(a < 0)) {
+			var b = this.items[a] || 0,
+			c = sc.inventory.getItem(a);
+			if (c.type == sc.ITEMS_TYPES.EQUIP) {
+				var d = -1,
+				e = -1;
+				switch (c.equipType) {
+					case sc.ITEMS_EQUIP_TYPES.HEAD:
+						d = this.equip.head;
+						break;
+					case sc.ITEMS_EQUIP_TYPES.ARM:
+						d = this.equip.leftArm;
+						e = this.equip.rightArm;
+						break;
+					case sc.ITEMS_EQUIP_TYPES.TORSO:
+						d = this.equip.torso;
+						break;
+					case sc.ITEMS_EQUIP_TYPES.FEET:
+						d = this.equip.feet
+				}
+				d >= 0 && d == a && b++;
+				e >= 0 && e == a && b++
+			}
+			return b
+		}
 	}
 });
