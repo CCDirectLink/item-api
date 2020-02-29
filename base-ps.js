f8aacb08b1b7028c2bc83c22b90da6e8e5eef554
Jonathan Sun <bakafish@berkeley.edu>
2020-02-29 07:49:03 -0800

    More prestart hijinks.

--- base-ps.js
+++ base-ps.js
@@ -21,18 +21,58 @@ sc.Inventory.inject({
 	addItem: function(a, c, d, e) {
 		var f = window.itemAPI.customItemToId[b];
 		f && (a = f);
-		if (!(a < 0)) {
-			this.items[a] = this.items[a] ? Math.min(this.items[a] + (c | 0), 99) : c | 0;
-			this._addNewItem(a);
-			sc.stats.addMap("items", "total", c);
-			sc.stats.addMap("items", a, c);
-			b.id = a;
-			b.amount = c;
-			b.skip = d;
-			b.cutscene = e;
-			sc.Model.notifyObserver(this, sc.PLAYER_MSG.ITEM_OBTAINED,
-			b)
-		}
-	}
+        if (!(a < 0)) {
+            this.items[a] = this.items[a] ? Math.min(this.items[a] + (c | 0), 99) : c | 0;
+            this._addNewItem(a);
+            sc.stats.addMap("items", "total", c);
+            sc.stats.addMap("items", a, c);
+            b.id = a;
+            b.amount = c;
+            b.skip = d;
+            b.cutscene = e;
+            sc.Model.notifyObserver(this, sc.PLAYER_MSG.ITEM_OBTAINED,
+                b)
+        }
+    },
+    isBuffID: function(b) {
+		return this.getItem(b).isBuff
+    },
+    isEquipID: function(b) {
+		return this.getItem(b).type == sc.ITEMS_TYPES.EQUIP
+    },
+    getItemNameWithIcon: function(b) {
+		b = this.getItem(b);
+		return !b ? "" : "\\i[" + (b.icon + this.getRaritySuffix(b.rarity || 0) || "item-default") + "]" + ig.LangLabel.getText(b.name)
+	},
+	getItemIcon: function(b) {
+		b = this.getItem(b);
+		if (!b)
+			return null;
+		return !b ? "item-default" : "\\i[" + (b.icon + this.getRaritySuffix(b.rarity || 0) || "item-default") + "]"
+	},
+	getItemDescription: function(b) {
+		b = this.getItem(b);
+		return b ? ig.LangLabel.getText(b.description) : ""
+	},
+	getItemRarity: function(b) {
+		b = this.getItem(b);
+		return !b ? null : b.rarity
+	},
+	getItemSubType: function(b) {
+		b = this.getItem(b);
+		return !b ? null : b.equipType
+	},
+	isConsumable: function(b) {
+		b = this.getItem(b);
+		return !b ? false : b.type == sc.ITEMS_TYPES.CONS
+	},
 });
 
+// Devs be violating abstraction barriers I swear to god.
+sc.ItemContent.inject({
+	init: function(b, a) {
+		var c = window.itemAPI.customItemToId[b];
+		c && (b = c);
+		this.parent(b, a);
+	}
+});
